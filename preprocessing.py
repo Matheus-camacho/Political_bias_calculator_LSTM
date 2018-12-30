d5688396e995d8c5e2654e9f5443211e1c366ea3
Matheus Camacho <6325656+Matheus-camacho@users.noreply.github.com>
2018-12-30 21:45:46 -0200

    Add files via upload

---
+++ preprocessing.py
@@ -0,0 +1,14 @@
+embeddings_index = dict()
+f = open('glove.twitter.27B.25d.txt')
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+pca_result = pca.fit_transform(df[feat_cols].values)
+df['pca-one'] = pca_result[:,0]
+df['pca-two'] = pca_result[:,1] 
